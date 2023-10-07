035d1ada999554656b1aea33b2d573d4475402ac
Chetnaya Kalyan <chetnaya.kalyan@gmail.com>
2023-10-07 13:53:44 +0530

    fix: inputHandler.cs updates

--- Escape the maze/Assets/Script/InputHandler.cs
+++ Escape the maze/Assets/Script/InputHandler.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class InputHandler : MonoBehaviour
 {
-    public void Button(int sceneIndex)
+    public GameObject introCanvas;
+    public GameObject player;
+    void Start()
     {
-        SceneManager.LoadScene(sceneIndex);
+        introCanvas.SetActive(true);
+        player.SetActive(false);
     }
+    public void YesButton()
+    {
+        introCanvas.SetActive(false);
+        player.SetActive(true);
+    }
+
     public void Exit()
     {
         #if UNITY_EDITOR
@@ -19,3 +28,22 @@ public class InputHandler : MonoBehaviour
     }
     
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class InputHandler : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
