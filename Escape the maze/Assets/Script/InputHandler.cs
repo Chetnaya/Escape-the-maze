519ebe03cf30f4caaf297bf3aa16d4353a1d6855
Chetnaya Kalyan <chetnaya.kalyan@gmail.com>
2023-10-07 13:43:47 +0530

    feat: add InputHandler.cs

---
+++ Escape the maze/Assets/Script/InputHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputHandler : MonoBehaviour
+{
+    public void Button(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+    
+}
