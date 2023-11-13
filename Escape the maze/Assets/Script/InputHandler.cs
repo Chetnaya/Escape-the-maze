bb38b87a1586d644ec779b58bcc630be91464c1f
Chetnaya Kalyan <chetnaya.kalyan@gmail.com>
2023-11-13 20:29:23 +0530

    chore: ProjectSettings.asset update

--- Escape the maze/Assets/Script/InputHandler.cs
+++ Escape the maze/Assets/Script/InputHandler.cs
@@ -50,10 +50,15 @@ public class InputHandler : MonoBehaviour
 
     public void Exit()
     {
-        #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-        #else
-        Application.Quit();
-        #endif
-    }
+#if (UNITY_EDITOR || DEVELOPMENT_BUILD)
+    Debug.Log(this.name+" : "+this.GetType()+" : "+System.Reflection.MethodBase.GetCurrentMethod().Name); 
+#endif
+#if (UNITY_EDITOR)
+    UnityEditor.EditorApplication.isPlaying = false;
+#elif (UNITY_STANDALONE) 
+    Application.Quit();
+#elif (UNITY_WEBGL)
+    Application.OpenURL("about:blank");
+#endif
+  }
 }
